46ae476b2a8da559c10f5467033954300dc9ee65
Kalesanwo <kalesanwoyinka99@gmail.com>
2024-08-22 17:39:22 +0100

    .

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/https://github.com/leylow99/https---github.com-leylow-URLShortener",
+  base: "//https:/github.com/leylow99/https---github.com-leylow-URLShortener/src/main.jsx",
 })
